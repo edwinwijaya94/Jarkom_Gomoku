c2b8404baf4793d2ad699c93fd30325fd5ff1458
vickonovianto <vicko1011@gmail.com>
2015-12-15 10:14:10 +0700

    coba isi tabel

--- GomokuGUI/src/gomokugui/FXMLDocumentRoomController.java
+++ GomokuGUI/src/gomokugui/FXMLDocumentRoomController.java
@@ -11,15 +11,20 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.application.Platform;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.HPos;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -29,6 +34,7 @@ import javafx.scene.control.ToolBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -49,12 +55,52 @@ public class FXMLDocumentRoomController implements Initializable {
     private Button changeCharacterButton;
     @FXML
     private Button startGameButton;
-    @FXML
-    private TableView<?> playersList;
     
     //private ObjectProperty<DataInputStream> dis = new SimpleObjectProperty<DataInputStream>(FXMLDocumentStartController.in);
     private String serverMessage;
     public static StringProperty str = new SimpleStringProperty();
+    @FXML
+    private TableView<?> playersNo;
+    @FXML
+    private TableView<?> playersName;
+    @FXML
+    private TableView<?> playersChar;
+    @FXML
+    private Label no1;
+    @FXML
+    private Label name1;
+    @FXML
+    private Label char1;
+    @FXML
+    private Label no2;
+    @FXML
+    private Label no3;
+    @FXML
+    private Label no4;
+    @FXML
+    private Label no5;
+    @FXML
+    private Label name2;
+    @FXML
+    private Label name3;
+    @FXML
+    private Label name4;
+    @FXML
+    private Label name5;
+    @FXML
+    private Label char2;
+    @FXML
+    private Label char3;
+    @FXML
+    private Label char4;
+    @FXML
+    private Label char5;
+    
+    private Label[] playersNumbers = new Label[5];
+    private Label[] playersNames = new Label[5];
+    private Label[] playersChars = new Label[5];
+    @FXML
+    private GridPane gridPane;
 
     /**
      * Initializes the controller class.
@@ -62,7 +108,7 @@ public class FXMLDocumentRoomController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        str.addListener(new ChangeListener<String>() {
+        /*str.addListener(new ChangeListener<String>() {
             
 
             @Override
@@ -71,7 +117,60 @@ public class FXMLDocumentRoomController implements Initializable {
                 
              
             }
-        });
+        });*/
+        
+        Task task2 = new Task<Void>() {
+            @Override
+            public Void call() {
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                
+                Platform.runLater(new Runnable() {
+                    @Override public void run() {
+                      playersNo.setPlaceholder(new Label(""));
+                      playersName.setPlaceholder(new Label(""));
+                      playersChar.setPlaceholder(new Label(""));
+                      gridPane.setGridLinesVisible(true);
+                    }  
+                });
+                
+                ObservableList<Node> childrens = gridPane.getChildren();
+                System.out.println(childrens.size());
+                for(Node node : childrens) {
+                    if (GridPane.getColumnIndex(node) == null) {
+                        System.out.println("masuk");
+                        if (GridPane.getRowIndex(node) != null) {
+                            playersNumbers[GridPane.getRowIndex(node)-1] = (Label)node;
+                        }
+                    }
+                    else if (GridPane.getColumnIndex(node) == 1) {
+                        if (GridPane.getRowIndex(node) != null)
+                            playersNames[GridPane.getRowIndex(node)-1] = (Label)node;
+                    }
+                    else if (GridPane.getColumnIndex(node) == 2) {
+                        if (GridPane.getRowIndex(node) != null)
+                            playersChars[GridPane.getRowIndex(node)-1] = (Label)node;
+                    }
+                }
+                
+                /*Platform.runLater(new Runnable() {
+                    @Override public void run() {  
+                      playersNumbers[0].setText("1");
+                      playersNames[0].setText("p1");
+                      String imglocation = "img/small/crop/" + 1 + ".png";
+                      Image image = new Image(getClass().getResourceAsStream(imglocation));
+                      ImageView iv = new ImageView(image);
+                      playersChars[0].setGraphic(iv);
+                    }  
+                });*/
+                         
+                return null;
+            }
+        };
+        new Thread(task2).start();
         
          //str.set("a");
         
--- GomokuGUI/src/gomokugui/FXMLDocumentRoomsController.java
+++ GomokuGUI/src/gomokugui/FXMLDocumentRoomsController.java
@@ -8,15 +8,24 @@ package gomokugui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -27,16 +36,54 @@ import javafx.stage.Stage;
  */
 public class FXMLDocumentRoomsController implements Initializable {
 
+    public static class Room {
+        private SimpleIntegerProperty roomNo;
+        private SimpleStringProperty roomName;
+        private SimpleIntegerProperty players;
+        
+        private Room(int roomNo, String roomName, int players) {
+            this.roomNo = new SimpleIntegerProperty(roomNo);
+            this.roomName = new SimpleStringProperty(roomName);
+            this.players = new SimpleIntegerProperty(players);
+        }
+        
+        public int getRoomNo() {
+            return roomNo.get();
+        }
+        
+        public String getRoomName() {
+            return roomName.get();
+        }
+        
+        public int getPlayers() {
+            return players.get();
+        }
+        
+        public void setRoomNo(int roomNo) {
+            this.roomNo.set(roomNo);
+        }
+        
+        public void setRoomName(String roomName) {
+            this.roomName.set(roomName);
+        }
+        
+        public void setPlayers(int players) {
+            this.players.set(players);
+        }
+    }
+    
     @FXML
     private Button homeButton;
     @FXML
-    private TableView<?> roomsList;
+    private TableView<Room> roomsList;
     @FXML
     private Button createRoomButton;
     @FXML
     private Button joinRoomButton;
     @FXML
     private Label roomTitle;
+    
+    private ObservableList<Room> data = FXCollections.observableArrayList();
 
     /**
      * Initializes the controller class.
@@ -44,6 +91,37 @@ public class FXMLDocumentRoomsController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+         Task task2 = new Task<Void>() {
+            @Override
+            public Void call() {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                
+                ObservableList<TableColumn<Room, ?>> columns = (ObservableList<TableColumn<Room, ?>>) roomsList.getColumns();
+                for (TableColumn column : columns) {
+                    //System.out.println(column.getText());
+                    if (column.getText().equals("No.")) {
+                        column.setCellValueFactory(new PropertyValueFactory<Room, String>("roomNo"));
+                        column.setStyle("-fx-alignment: CENTER;");
+                    } else if (column.getText().equals("Room Name")) {
+                        column.setCellValueFactory(new PropertyValueFactory<Room, String>("roomName"));                        
+                    } else if (column.getText().equals("Players")) {
+                        column.setCellValueFactory(new PropertyValueFactory<Room, String>("players"));                        
+                        column.setStyle("-fx-alignment: CENTER;");
+                    }
+                }
+                
+                roomsList.setItems(data);
+                
+                //data.add(new Room(1, "a", 0));
+                         
+                return null;
+            }
+        };
+        new Thread(task2).start();
     }    
 
     @FXML
@@ -76,7 +154,6 @@ public class FXMLDocumentRoomsController implements Initializable {
 
     @FXML
     private void joinRoom(MouseEvent event) throws IOException {
-        
         Parent root;
         Stage dialogStage = new Stage();
         root = FXMLLoader.load(getClass().getResource("FXMLDocumentChooseChar.fxml"));
